5afba6454c0df175585c60a71f8d86f66c4a96a4
Vadim Belyaev <vadimbelyaev@gmail.com>
2022-07-13 02:54:04 +0400

    Create empty app

---
+++ TbilisiCleanups/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TbilisiCleanups
+//
+//  Created by Vadim Belyaev on 13.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TbilisiCleanupsUITests/TbilisiCleanupsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TbilisiCleanupsUITestsLaunchTests.swift
+//  TbilisiCleanupsUITests
+//
+//  Created by Vadim Belyaev on 13.07.2022.
+//
+
+import XCTest
+
+class TbilisiCleanupsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
