25d81055188fb0a193cceb0c75f527a8b4e2dcfc
Vadim Belyaev <vadimbelyaev@gmail.com>
2022-07-14 00:38:20 +0400

    Re-add Firebase, change deployment target back to iOS 15.0

--- TbilisiCleanups/TbilisiCleanupsApp.swift
+++ TbilisiCleanups/TbilisiCleanupsApp.swift
@@ -5,10 +5,16 @@
 //  Created by Vadim Belyaev on 14.07.2022.
 //
 
+import Firebase
 import SwiftUI
 
 @main
 struct TbilisiCleanupsApp: App {
+
+    init() {
+        FirebaseApp.configure()
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
