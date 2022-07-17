15ad18037b5afd0b243486301130f13b16cf7fa0
Vadim Belyaev <vadimbelyaev@gmail.com>
2022-07-17 12:01:16 +0400

    Move ReportDraft state to a separate file

--- TbilisiCleanups/State/AppState.swift
+++ TbilisiCleanups/State/AppState.swift
@@ -1,33 +1,6 @@
-import MapKit
-import SwiftUI
+import Foundation
 
 final class AppState: ObservableObject {
     var currentDraft: ReportDraft = .empty
 }
 
-class ReportDraft: ObservableObject {
-    @Published var locationRegion = MKCoordinateRegion(
-        center: CLLocationCoordinate2D(
-            latitude: 42.182_724,
-            longitude: 43.523_521
-        ),
-        latitudinalMeters: 600_000,
-        longitudinalMeters: 600_000
-    )
-    @Published var placeDescription: String = ""
-    @Published var medias: [PlaceMedia] = []
-
-    static let empty = ReportDraft()
-
-    func remove(media: PlaceMedia) {
-        guard let index = medias.firstIndex(where: { $0.id == media.id }) else {
-            return
-        }
-        medias.remove(at: index)
-    }
-}
-
-struct PlaceMedia: Identifiable {
-    let id: String
-    let itemProvider: NSItemProvider
-}
---
+++ TbilisiCleanups/State/ReportDraft.swift
@@ -0,0 +1,28 @@
+import MapKit
+
+class ReportDraft: ObservableObject {
+    @Published var locationRegion = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(
+            latitude: 42.182_724,
+            longitude: 43.523_521
+        ),
+        latitudinalMeters: 600_000,
+        longitudinalMeters: 600_000
+    )
+    @Published var placeDescription: String = ""
+    @Published var medias: [PlaceMedia] = []
+
+    static let empty = ReportDraft()
+
+    func remove(media: PlaceMedia) {
+        guard let index = medias.firstIndex(where: { $0.id == media.id }) else {
+            return
+        }
+        medias.remove(at: index)
+    }
+}
+
+struct PlaceMedia: Identifiable {
+    let id: String
+    let itemProvider: NSItemProvider
+}
