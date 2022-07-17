6a8247104a7214135c4f40ed90226cbe7cf97921
Vadim Belyaev <vadimbelyaev@gmail.com>
2022-07-17 11:59:17 +0400

    Move the report flow to a sheet

--- TbilisiCleanups/Views/ReportStartView/ReportStartView.swift
+++ TbilisiCleanups/Views/ReportStartView/ReportStartView.swift
@@ -2,45 +2,20 @@ import SwiftUI
 
 struct ReportStartView: View {
 
+    @State private var reportScreenPresented = false
+
     var body: some View {
         NavigationView {
             VStack {
                 ScrollView {
-                    VStack(alignment: .leading, spacing: 24) {
-                        Text("Report a littered place in 3 steps:")
-                            .font(.title)
-                            .padding(.bottom, 24)
-                        HStack {
-                            Image(systemName: "photo.on.rectangle.angled")
-                                .frame(minWidth: 40)
-                            Text("Choose some photos or videos")
-                        }
-                        .font(.title3)
-                        HStack {
-                            Image(systemName: "mappin.and.ellipse")
-                                .frame(minWidth: 40)
-                            Text("Tell us the location")
-                        }
-                        .font(.title3)
-                        HStack {
-                            Image(systemName: "rectangle.and.pencil.and.ellipsis")
-                                .frame(minWidth: 40)
-                            Text("Add a text description")
-                        }
-                        .font(.title3)
-
-                        Text("Once submitted, your report will be reviewed by our moderators.")
-                            .padding(.top, 24)
-
-                        Text("Thank you for contributing to a cleaner country!")
-                    }
+                    bodyText
                 }
                 Spacer()
 
                 HStack {
                     Spacer()
-                    NavigationLink {
-                        ReportPhotosView()
+                    Button {
+                        reportScreenPresented = true
                     } label: {
                         Text("Start")
                             .frame(maxWidth: 300)
@@ -54,10 +29,46 @@ struct ReportStartView: View {
             .padding()
             .navigationTitle("New Report")
         }
+        .sheet(isPresented: $reportScreenPresented) {
+            NavigationView {
+                ReportPhotosView()
+            }
+        }
         .navigationViewStyle(.stack)
         .tabItem {
             Image(systemName: "square.and.pencil")
             Text("New Report")
+        }
+    }
+
+    private var bodyText: some View {
+        VStack(alignment: .leading, spacing: 24) {
+            Text("Report a littered place in 3 steps:")
+                .font(.title)
+                .padding(.bottom, 24)
+            HStack {
+                Image(systemName: "photo.on.rectangle.angled")
+                    .frame(minWidth: 40)
+                Text("Choose some photos or videos")
+            }
+            .font(.title3)
+            HStack {
+                Image(systemName: "mappin.and.ellipse")
+                    .frame(minWidth: 40)
+                Text("Tell us the location")
+            }
+            .font(.title3)
+            HStack {
+                Image(systemName: "rectangle.and.pencil.and.ellipsis")
+                    .frame(minWidth: 40)
+                Text("Add a text description")
+            }
+            .font(.title3)
+
+            Text("Once submitted, your report will be reviewed by our moderators.")
+                .padding(.top, 24)
+
+            Text("Thank you for contributing to a cleaner country!")
         }
     }
 }
